2cebcb883c73941581d7215fd164e8a983ba2cda
sujeong11 <qalzm7351@gmail.com>
2022-12-17 16:16:14 +0900

    feat: code로 access token 발급

--- src/main/java/com/sujeong/oauth20/controller/OauthController.java
+++ src/main/java/com/sujeong/oauth20/controller/OauthController.java
@@ -1,12 +1,20 @@
 package com.sujeong.oauth20.controller;
 
 import com.sujeong.oauth20.config.auth.PrincipalDetails;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OauthController {
@@ -37,5 +45,36 @@ public class OauthController {
     public String test(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         System.out.println("PrincipalDetails" + principalDetails.getUser());
         return "한 타입으로 세션정보 확인";
+    }
+
+    @GetMapping("/auth/kakao/callback")
+    public String kakaoCallback(@RequestParam String code) {
+
+        String reqUrl = "https://kauth.kakao.com/oauth/token";
+        RestTemplate rt = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpBody 오브젝트 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "94ff8c81132286d85f870b98a437f2b3");
+        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+        params.add("code", code);
+
+        // HttpHeader와 HttpBody를 하나의 오브젝트로 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        // Post로 Http 요청
+        ResponseEntity response = rt.exchange(
+                reqUrl,
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        return "카카오 인증 완료 후 응답 - " + response;
     }
 }
